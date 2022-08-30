6eaa79613d03f4d776d94d64ee59b52f0d30ee40
benmarita96@gmail.com <benmarita96@gmail.com>
2022-08-30 09:44:09 -0400

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+// Your code goes here
+ 
+document.addEventListener("DOMContentLoaded",function(){
+    let main = document.querySelector('#text');
+
+     main.textContent = "This is really cool!";
+
+ }
+ );
+ 
